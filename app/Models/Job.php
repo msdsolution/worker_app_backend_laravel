21884a8723b9f06816083df4babc87bb7d35c9c7
jdeepshan <86647317+jdeepshan@users.noreply.github.com>
2024-07-28 17:45:30 +0530

    update job.php

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -42,7 +42,7 @@ class Job extends Model
 
     public function complaint()
     {
-        return $this->hasOne(Job::class, 'job_id');
+        return $this->hasOne(Job::class, 'id');
     }
 
     //  public function jobServices()
