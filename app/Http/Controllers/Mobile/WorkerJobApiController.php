b946c47c7215ed3daddedc09b8bad487a92b6b86
jdeepshan <86647317+jdeepshan@users.noreply.github.com>
2024-09-02 00:00:51 +0530

    updated workerJobApi finished job

--- app/Http/Controllers/Mobile/WorkerJobApiController.php
+++ app/Http/Controllers/Mobile/WorkerJobApiController.php
@@ -86,7 +86,7 @@ class WorkerJobApiController extends Controller
         $job_statuses = [4, 5];
         $query = Job::leftJoin('worker_payment', 'job.id', '=', 'worker_payment.job_id')
             ->leftJoin('worker_payment_attachment', 'worker_payment.id', '=', 'worker_payment_attachment.worker_payment_id')
-            ->where('job.status', $job_statuses)  // Filter jobs with status 4
+            ->whereIn('job.status', $job_statuses)  // Filter jobs with status 4
             ->where('job.worker_id', $user->id)  // Ensure worker_id matches
             ->select('job.*',  DB::raw('CONCAT("' . url('storage') . '/", worker_payment_attachment.file_path) as worker_payment_attachment_url'), 'worker_payment.amount', DB::raw('COALESCE(worker_payment.status, 0) as worker_payment_status'))
             ->orderBy('job.created_at', 'desc');
